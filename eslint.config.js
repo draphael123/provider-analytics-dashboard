85406eef43630b8b721e31571e687616215961d7
draphael123 <draphael123@users.noreply.github.com>
2026-01-27 10:15:16 -0500

    Fix ESLint dependency conflict: Upgrade TypeScript ESLint to v8 for ESLint 9 compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,9 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules', '*.config.js'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
